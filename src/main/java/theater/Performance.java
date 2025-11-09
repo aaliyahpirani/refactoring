4c60c86af6a6a4e45600d9e37d4829e69076532f
aaliyahpirani <aaliyah.happy@gmail.com>
2025-11-09 14:28:36 -0500

    Task 1.1 completed

--- src/main/java/theater/Performance.java
+++ src/main/java/theater/Performance.java
@@ -5,11 +5,24 @@ package theater;
  */
 public class Performance {
 
-    public String playID;
-    public int audience;
+    private String playID;
+    private int audience;
 
     public Performance(String playID, int audience) {
         this.playID = playID;
         this.audience = audience;
     }
+
+    /**
+     * Returns the ID of the play being performed.
+     * @return the play ID
+     */
+    public final String getplayID() {
+        return playID;
+    }
+
+    public final int getAudience() {
+        return audience;
+    }
 }
+
